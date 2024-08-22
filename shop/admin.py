fc03cabd93814ab3ff4df43e4244ef62652d1275
Abishake01 <abishake381@gmail.com>
2024-08-22 17:35:03 +0530

    Sliders Added

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Catagory
 from .models import Product
 
-admin.site.register(Catagory)
+#class CatagoryAdmin(admin.ModelAdmin):
+   # list_display=('name','image','description')
+    
+admin.site.register(Catagory)#,CatagoryAdmin
 admin.site.register(Product)
