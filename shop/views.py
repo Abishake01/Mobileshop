99bfcd0e67cd1df869bc384ea951a46e8a801bf5
Abishake01 <abishake381@gmail.com>
2024-08-29 22:46:42 +0530

    Category Show

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 
 def home(request):
-    return render(request,'shop/index.html')
+    catagory=Catagory.objects.filter(status=0)
+    return render(request,'shop/index.html',{'catagory':catagory})
 def register(request):
     return render(request,'shop/register.html')
