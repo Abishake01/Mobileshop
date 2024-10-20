835198f50a13519201ecad35e13c174c3dfac1a5
Abishake01 <abishake381@gmail.com>
2024-10-20 15:46:18 +0530

    Hosting

--- partha_shop/settings.py
+++ partha_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5k)n8(^!-_job1%nz)bo^^w%@w%az5yucgv61f_mxo9x9d56o$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh','*']
 
 
 # Application definition
